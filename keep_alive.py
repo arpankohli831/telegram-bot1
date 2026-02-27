e84e56fc727d19492f8139301d3a8d5643f37f09
arpankohli831 <arpankohli831@gmail.com>
2026-02-28 02:31:04 +0530

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask("")
+app = Flask("keep_alive_bot")
 
 @app.route("/")
 def home():
     return "Bot is alive"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Railway assigns this port
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     Thread(target=run).start()
